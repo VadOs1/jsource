4a90c3ceae8cdd88ae5550050f0f8be56f36fedc
Vadim Dissa <vadim.dissa@gmail.com>
2018-10-28 18:20:47 -0400

    Simplify init sql script for auth-server

--- auth-server/src/main/resources/db/migration/V1_1__init.sql
+++ auth-server/src/main/resources/db/migration/V1_1__init.sql
@@ -5,13 +5,12 @@ CREATE TABLE users (
 );
 
 CREATE TABLE authority (
-  id   BIGSERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  name VARCHAR(20) PRIMARY KEY
 );
 
 CREATE TABLE authorities (
-  username  VARCHAR(100) NOT NULL  REFERENCES users (username),
-  authority BIGINT       NOT NULL REFERENCES authority (id),
+  username  VARCHAR(100) NOT NULL REFERENCES users (username),
+  authority VARCHAR(20)  NOT NULL REFERENCES authority (name),
   PRIMARY KEY (username, authority)
 );
 
@@ -19,5 +18,5 @@ INSERT INTO authority (name) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
 
 INSERT INTO users
 VALUES  ('superkid-user', '$2a$10$.ClGiX/64Ki0tZJrAZbLzeoo5hAaCZVycBv9XdIWJdNmNL3WMX/6u', TRUE);
-INSERT INTO authorities VALUES ('superkid-user', 1);
-INSERT INTO authorities VALUES ('superkid-user', 2);
+INSERT INTO authorities VALUES ('superkid-user', 'ROLE_USER');
+INSERT INTO authorities VALUES ('superkid-user', 'ROLE_ADMIN');
