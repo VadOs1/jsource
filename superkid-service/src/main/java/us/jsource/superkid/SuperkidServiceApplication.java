78d63ad138ecece1e16616e0653facd01ecf364a
Vadim Dissa <vadim.dissa@gmail.com>
2018-11-05 18:19:13 -0500

    Add oauth2 security to superkid-service

--- superkid-service/src/main/java/us/jsource/superkid/SuperkidServiceApplication.java
+++ superkid-service/src/main/java/us/jsource/superkid/SuperkidServiceApplication.java
@@ -3,8 +3,10 @@ package us.jsource.superkid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableDiscoveryClient
+@EnableResourceServer
 @SpringBootApplication
 public class SuperkidServiceApplication {
     public static void main(String[] args) {
